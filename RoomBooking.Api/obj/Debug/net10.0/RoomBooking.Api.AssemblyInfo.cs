45536ece9bad821462298478c655431a2b038153
May-Sif <may.12may05@gamil.com>
2026-02-12 12:54:03 +0700

    simpan seperti semula

--- RoomBooking.Api/obj/Debug/net10.0/RoomBooking.Api.AssemblyInfo.cs
+++ RoomBooking.Api/obj/Debug/net10.0/RoomBooking.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoomBooking.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f08bd035ed54b0a47ab4c3bbf87a249e41c5dc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoomBooking.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoomBooking.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
